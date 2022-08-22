df06596565aee8e4e1536df4ba70e92b5c434c7e
LucasVieiraS <lucas.silva3759@etec.sp.gov.br>
2022-08-21 22:58:22 -0300

    fix: project banner

--- src/app/portfolio/projects/projects.component.ts
+++ src/app/portfolio/projects/projects.component.ts
@@ -12,9 +12,9 @@ export class ProjectsComponent implements OnInit {
       name: 'Machinery',
       description: 'Discord bot that assists in automation.',
       logo: 'fa-brands fa-discord',
-      image: 'https://images.unsplash.com/photo-1637984135921-301a7d39e3b7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c2F0dXJufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+      image: 'https://github.com/LucasVieiraS/Machinery/blob/main/src/img/logo.png?raw=true',
       repository: 'https://github.com/LucasVieiraS/Machinery',
-      last_commit: '???',
+      version: 'v2.0',
       main_branch: 'main',
     },
   ]
@@ -30,7 +30,7 @@ export class ProjectsComponent implements OnInit {
       .then(response => {
         response.json().then(json => {
           console.log(json);
-          this.repositories[this.repositories.indexOf(data)].last_commit = json.commit.commit.author.date
+          this.repositories[this.repositories.indexOf(data)].version = json.commit.commit.author.date
         });
       })
       .catch(error => {
