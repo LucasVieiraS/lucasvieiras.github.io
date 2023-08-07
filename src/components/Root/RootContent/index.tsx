b76d790f599be8a0a792e4a0a1f76334fa40c00f
Lucas <lucasvieirasilva.lpta@gmail.com>
2023-08-06 22:19:56 -0300

    refactor(deploy): project structure

--- src/components/Root/RootContent/index.tsx
+++
@@ -1,9 +0,0 @@
-import React, { ReactNode } from 'react'
-
-export default function RootContent({ children } : { children: ReactNode }) {
-  return (
-    <main className="w-full">
-        { children }
-    </main>
-  )
-}
--- src/util/editorConfig/projects.ts
+++
@@ -1,6 +0,0 @@
-export const projects = `
-    <h1>Projects</h1>
-    <p>
-        This portfolio is a project aswell.
-    </p>
-`;
